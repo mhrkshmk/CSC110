4ded2cb20404bf1dfa9a20eff0d3945d4ca193d1
Mehrkish Makouei <127551650+mhrkshmk@users.noreply.github.com>
2024-01-26 18:13:56 -0800

    Create assignment10.py

---
+++ Assignment10/assignment10.py
@@ -0,0 +1,152 @@
+import doctest
+from pet import Pet
+from date import Date
+import csv
+
+# represents a pet as (name, species)
+PetNameSpecies = tuple[str,str]
+
+# columns of values within input file row and within PetNameSpecies tuple
+NAME    = 0
+SPECIES = 1
+MONTH   = 2
+DAY     = 3
+YEAR    = 4
+
+def read_file(filename: str) -> list[Pet]:
+    ''' returns a list of Pets populated with data from filename
+    
+    Preconditions: filename exists.
+    If filename is not empty, each row has a single Pet's information
+    separated by commas with expected values at columns:
+    NAME, SPECIES, MONTH, DAY and YEAR.
+
+    >>> read_file('empty.csv')
+    []
+    >>> read_file('pet_data.csv')
+    [Pet('Rover', 'Dog', Date(12, 31, 2010)), \
+Pet('Red', 'Cat', Date(9, 15, 2016)), \
+Pet('Chewie', 'Hamster', Date(1, 23, 2009)), \
+Pet('Sam', 'Budgie', Date(3, 29, 1990)), \
+Pet('Ollie', 'Dog', Date(2, 8, 2009)), \
+Pet('Scout', 'Dog', Date(9, 15, 2016))]
+    '''
+    # TODO: complete this function
+    file = open(filename, 'r')
+    filereader = csv.reader(file)
+    pets = []
+    for row in filereader:
+        day = row[DAY]
+        month = row[MONTH]
+        year = row[YEAR]
+        dt = Date(month, day, year)
+        pet = Pet(row[0], row[1], dt)
+        pets.append(pet)
+    file.close()
+    return pets
+
+def find_pet(lopets: list[Pet], name: str) -> int:
+    ''' returns the position of pet with given name in lopets
+    Returns -1 if name not found
+    Returns the position of the first found if there >1 Pet with the given name
+    
+    Precondition: name must match case exactly
+    ie. 'rover' does not match 'Rover'
+
+    >>> find_pet([], 'Fred')
+    -1
+    >>> find_pet([Pet('Rover', 'Dog', Date(12, 22, 2020)), \
+Pet('Red', 'Cat', Date(1, 2, 2019))], 'Red')
+    1
+    >>> find_pet([Pet('Rover', 'Dog', Date(12, 22, 2020)), \
+Pet('Red', 'Cat', Date(1, 2, 2019))], 'Bowser')
+    -1
+    >>> find_pet([Pet('Red', 'Dog', Date(12, 22, 2020)), \
+Pet('Red', 'Cat', Date(1, 2, 2019))], 'Red')
+    0
+    '''
+    # TODO: complete this function
+    for index in range(len(lopets)):
+        pet = lopets[index]
+        if pet.get_name() == name:
+            return index
+    return -1
+
+def get_all_of_species(lopets: list[Pet], species: str) -> list[Pet]:
+    ''' returns a list of all pets of the given species.
+    Result list is not necessarily unique, if >1 Pet in lopets has the same name.
+    
+    Precondition: species must match case exactly
+    ie. 'dog' does not match 'Dog'
+    
+    >>> pets = [Pet('Rover', 'Dog', Date(12, 31, 2010)), \
+Pet('Red', 'Cat', Date(9, 15, 2016)), \
+Pet('Chewie', 'Hamster', Date(1, 23, 2009)), \
+Pet('Sam', 'Budgie', Date(3, 29, 1990)), \
+Pet('Ollie', 'Dog', Date(2, 8, 2009)), \
+Pet('Scout', 'Dog', Date(9, 15, 2016))]
+
+    >>> get_all_of_species([], 'Dog')
+    []
+    >>> get_all_of_species(pets, 'Dog')
+    [Pet('Rover', 'Dog', Date(12, 31, 2010)), \
+Pet('Ollie', 'Dog', Date(2, 8, 2009)), \
+Pet('Scout', 'Dog', Date(9, 15, 2016))]
+    >>> get_all_of_species(pets, 'Tiger')
+    []
+    >>> get_all_of_species(pets, 'Hamster')
+    [Pet('Chewie', 'Hamster', Date(1, 23, 2009))]
+    '''
+    # TODO: complete this function
+    pets = []
+    for pet in lopets:
+        if pet.get_species() == species:
+            pets.append(pet)
+    return pets
+
+def get_latest_birthdate(lopets: list[Pet]) -> Date:
+    ''' returns the latest Date of all birthdates of Pet instances in lopets
+    Precondition: lopets is not empty
+    >>> pets = [Pet('Rover', 'Dog', Date(12, 31, 2010)), \
+Pet('Red', 'Cat', Date(9, 15, 2016)), \
+Pet('Chewie', 'Hamster', Date(1, 23, 2009)), \
+Pet('Sam', 'Budgie', Date(3, 29, 1990)), \
+Pet('Ollie', 'Dog', Date(2, 8, 2009)), \
+Pet('Scout', 'Dog', Date(9, 15, 2016))]
+
+    >>> get_latest_birthdate([Pet('Rover', 'Dog', Date(12, 31, 2010))])
+    Date(12, 31, 2010)
+    >>> get_latest_birthdate(pets)
+    Date(9, 15, 2016)
+    '''
+    # TODO: complete this function
+    dt = Date(-1, -1, -1)
+    for pet in lopets:
+        birthdate = pet.get_birthdate()
+        if birthdate.is_after(dt):
+            dt = birthdate
+    return dt
+
+
+def get_youngest_pets(lopets: list[Pet]) -> list[PetNameSpecies]:
+    ''' returns a list of PetNameSpecies of all the youngest pets in lopets
+    >>> pets = [Pet('Rover', 'Dog', Date(12, 31, 2010)), \
+Pet('Red', 'Cat', Date(9, 15, 2016)), \
+Pet('Chewie', 'Hamster', Date(1, 23, 2009)), \
+Pet('Sam', 'Budgie', Date(3, 29, 1990)), \
+Pet('Ollie', 'Dog', Date(2, 8, 2009)), \
+Pet('Scout', 'Dog', Date(9, 15, 2016))]
+    >>> get_youngest_pets([])
+    []
+    >>> get_youngest_pets(pets)
+    [('Red', 'Cat'), ('Scout', 'Dog')]
+    '''
+    # TODO: complete this function
+    dt = get_latest_birthdate(lopets)
+    pets = []
+    for pet in lopets:
+        if pet.get_birthdate() == dt:
+            name = pet.get_name()
+            species = pet.get_species()
+            pets.append(tuple([name, species]))
+    return pets
